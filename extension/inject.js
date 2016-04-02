e5df01a7ecaf50169d5faea945c84aec4b051546
melaniehoff <hoffmelanie@gmail.com>
2016-04-02 18:56:23 -0400

    audio

--- extension/inject.js
+++ extension/inject.js
@@ -1,14 +1,13 @@
 
 $( document ).ready(function() {
 	console.log('injecting works');
-
+	var audioFile = new Audio('http://artdelicorp.com/audio/browser-surprise-audio.m4a');
+	audioFile.play();
 	setTimeout(function(){ browser_surprise(); }, 3000);
 });
 
 
 function browser_surprise(){
-	var audioFile = new Audio('browser_surprise.m4a');
-	audio.play()
 
 	window.open('http://leoneckert.com/','_blank');
 }
