1409e35272e4a763092c76109a42a8f61d72d901
Leon Eckert <eckertleon@gmail.com>
2016-04-03 18:24:01 -0400

    added comments in inject.js

--- extension/inject.js
+++ extension/inject.js
@@ -1,28 +1,34 @@
 $( document ).ready(function() {
-	var xhttp = new XMLHttpRequest();
-	var currentUrl = window.location.href;
-	xhttp.open("GET", "http://localhost:3000/sendAFile?sendurl=" + currentUrl, true);
-	xhttp.send()
+	
+	// this happens once, when a new site is opened 
+	// (ideally this would not happen on sites opened by BS):
+	sendCurrentUrlToBSserver();
+	
 
-	console.log(currentUrl);
-	console.log('injecting works');
-
-
+	// right now a new sound plays on EVERY page that is opened, ideally only happening on 
+	// pages BS opens
 	var audioFile = new Audio('http://artdelicorp.com/audio/browser-surprise-audio.m4a');
 	audioFile.play();
 
+	// very site runs the browser surprise function right now.
+	// ideally this should happen not in injected scripts, but in the popup.js script
+	// the problem is that only runs when the pugin button is clicked
 	setTimeout(function(){ browser_surprise(); }, 10000);
 
 });
 
 
 function browser_surprise(){
-
+	// right now opens a placeholder site, in the end it will request a url from the server
+	// that url is then opened
 	window.open('http://artdelicorp.com/img2/browser-surprise.png','_blank');
 
-	//i too made a comment (leon)
-
-	//i made a comment
+}
 
 
+function sendCurrentUrlToBSserver(){
+	var xhttp = new XMLHttpRequest();
+	var currentUrl = window.location.href;
+	xhttp.open("GET", "http://localhost:3000/sendAFile?sendurl=" + currentUrl, true);
+	xhttp.send();
 }
