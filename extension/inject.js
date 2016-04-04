d0f765e2364e30a91d9bc59a278925967db50f4e
Leon Eckert <eckertleon@gmail.com>
2016-04-03 23:03:34 -0400

    very far, few details like making sure no webvsite are shared twice, well unless we decide we want that, that would result in a crazy criculation of pages

--- extension/inject.js
+++ extension/inject.js
@@ -1,21 +1,16 @@
+var serverIP = "172.16.249.148"
+// var serverIP = "localhost"
 var newURL = "";
 $( document ).ready(function() {
 	
-	// this happens once, when a new site is opened 
-	// (ideally this would not happen on sites opened by BS):
 	sendCurrentUrlToBSserver();
 
 	playSoundIfBrowserSurprise();
-	
-	// very site runs the browser surprise function right now.
-	// ideally this should happen not in injected scripts, but in the popup.js script
-	// the problem is that only runs when the pugin button is clicked
-
 
 	var timeLastAction = new Date().getTime() / 1000;
+	
 	setInterval(function() {
 		askServerForNewURL();
-		
 		// now let's check if the url is useful (thats the case if the server decides to send one back,
 		// has to do with the timer on the server);
 		if(newURL.length > 0){
@@ -32,15 +27,19 @@ $( document ).ready(function() {
 
 });
 
+function sendCurrentUrlToBSserver(){
+	var xhttp = new XMLHttpRequest();
+	var currentUrl = window.location.href;
+	xhttp.open("GET", "http://" + serverIP + ":3000/sendAFile?sendurl=" + currentUrl, true);
+	xhttp.send();
+}
+
 function playSoundIfBrowserSurprise(){
 	var currentUrl = window.location.href;
 	if (currentUrl.length > 24){
 		var endOfURL = currentUrl.slice(currentUrl.length - 24, currentUrl.length);
 		console.log("endOfURL");
 		if(endOfURL == "####Browser-Surprise####"){
-			// right now a new sound plays on EVERY page that is opened, ideally only happening on 
-			// pages BS opens
-			// still not resolved
 			var audioFile = new Audio('http://artdelicorp.com/audio/browser-surprise-audio.m4a');
 			audioFile.play();
 		}
@@ -50,38 +49,17 @@ function playSoundIfBrowserSurprise(){
 function askServerForNewURL(){
 	// learned from here: http://stackoverflow.com/a/6012543
 	xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET","http://localhost:3000/getNewUrl", true);
+	xmlhttp.open("GET","http://" + serverIP + ":3000/getNewUrl", true);
 	xmlhttp.onreadystatechange=function(){
 	    if (xmlhttp.readyState==4 && xmlhttp.status==200){
 	        newURL = String(xmlhttp.responseText);
 	    }
 	}
 	xmlhttp.send();
-
 }
 
 function browser_surprise(url_to_open){
-	
-
-
-	// right now opens a placeholder site, in the end it will request a url from the server
-	// that url is then opened
-	// we probably can't limit the amount if call to the server (because popup.js only call on click 
-	// and inject runs on every page indipendently).
-	// we can make many calls, but maybe only send a url back to open 
-	// wheneevr an interval is reached for that IP address.
-
 	window.open(url_to_open,'_blank');
-
-
 }
 
 
-function sendCurrentUrlToBSserver(){
-	// this sends the url the client is on. On the server, this information is paired with 
-	// the client's ip addresse
-	var xhttp = new XMLHttpRequest();
-	var currentUrl = window.location.href;
-	xhttp.open("GET", "http://localhost:3000/sendAFile?sendurl=" + currentUrl, true);
-	xhttp.send();
-}
--- server_side/index.js
+++ server_side/index.js
@@ -1,65 +1,130 @@
-// To setup: (1) cd in terminal to this folder, (2) type "npm install express"
-// documentation: http://expressjs.com
-
-// To run: while inside this folder in terminal, type "node server.js"
-
-// Include express module and initialize app
 var express = require('express');
-
 var app = express();
 var http = require('http').Server(app);
-
 // Start server listening on port 3000
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Browser Surprise listening on port 3000!');
 });
-
-
 
 app.get('/', function (req, res) {
   res.sendfile('public/index.html');
 });
 
+
+//                    _             _        _         
+//  _ __ ___  ___ ___(_)_   _____  | |_ __ _| |__  ___ 
+// | '__/ _ \/ __/ _ \ \ \ / / _ \ | __/ _` | '_ \/ __|
+// | | |  __/ (_|  __/ |\ V /  __/ | || (_| | |_) \__ \
+// |_|  \___|\___\___|_| \_/ \___|  \__\__,_|_.__/|___/
+
+var sitesByUser = {}
 app.get('/sendAFile', function (req, res) {
-  res.send(req.query.sendurl); 
-  console.log("user with ip " + req.connection.remoteAddress + " went to: " + req.query.sendurl)
-  // this is the users ip address
-  // res.send(req.connection.remoteAddress);
+  // res.send(req.query.sendurl); 
+  var clientIP = req.connection.remoteAddress;
+	if (!sitesByUser[clientIP]){ 	// check if device already communicated with:
+		sitesByUser[clientIP] = {}
+		sitesByUser[clientIP][req.query.sendurl] = 1;
+	}else{
+		sitesByUser[clientIP][req.query.sendurl] = 1;
+	}
+	console.log(sitesByUser);
 });
 
+//   __                  _   _                 
+//  / _|_   _ _ __   ___| |_(_) ___  _ __  ___ 
+// | |_| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+// |  _| |_| | | | | (__| |_| | (_) | | | \__ \
+// |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+
+function urlOtherThanClients(clientsIP){
+	for(ip in sitesByUser){
+		for(site in sitesByUser[ip]){
+			if(site){
+				if(ip != clientsIP){
+					return true;
+				}
+			}
+		}
+	}
+	// console.log('only client ' + clientsIP + " is active.");
+	return false;
+}
+
+// from: http://stackoverflow.com/a/2532251
+function pickRandomProperty(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+}
+
+function randomUrlFromOtherClient(clientsIP){
+	var ranUrl = "";
+	var ipAssociated = clientsIP;
 
 
+	while(ranUrl == "" && ipAssociated == clientsIP){
+		ipAssociated = pickRandomProperty(sitesByUser);
+		console.log('picked this ip: ' + ipAssociated);
+		ranUrl = pickRandomProperty(sitesByUser[ipAssociated]);
+		console.log('picked this site: ' + ranUrl);
+	}
+	var urlIPpair = [];
+	urlIPpair.push(ranUrl);
+	urlIPpair.push(ipAssociated);
+	console.log('created this pack: ' + urlIPpair);
+	return urlIPpair;
+}
+
+
+
+//                     _              _ 
+//  ___  ___ _ __   __| |  _   _ _ __| |
+// / __|/ _ \ '_ \ / _` | | | | | '__| |
+// \__ \  __/ | | | (_| | | |_| | |  | |
+// |___/\___|_| |_|\__,_|  \__,_|_|  |_|
+                                     
 
 var deviceNewURLtimer = {}
 var browserSurpriseInterval = 20; //in seconds
-
-
 app.get('/getNewUrl', function (req, res) {
 	var URLtoSend = "";
 	var clientIP = req.connection.remoteAddress;
 	// check if device already communicated with:
 	if (!deviceNewURLtimer[clientIP]){
-		// if not, add the IP address and current time
 		deviceNewURLtimer[clientIP] = new Date().getTime() / 1000;
-		// and return a Url to open as a browser surprise:
-		URLtoSend = "http://leoneckert.com####Browser-Surprise####";
-		console.log("[+] sending this to the client: " + URLtoSend);
+		
+		// send back a new url (like below)!
+
+		// if(urlOtherThanClients(clientIP)){
+		// 	console.log('Yes');
+		// }
+
 	}else{
-		// if allready communicated with the client check if it's time to send a new website
 		var currentTime = new Date().getTime() / 1000;
+		
 		if(currentTime - deviceNewURLtimer[clientIP] >= browserSurpriseInterval){
-			URLtoSend = "http://www.artdelicorp.com/img2/browser-surprise.png####Browser-Surprise####";
-			console.log("[+] sending this to the client: " + URLtoSend);
+			// console.log('[?] client ' + clientIP + ' would get an url now. ' + currentTime - deviceNewURLtimer[clientIP] + " seconds since the last time.")
+			if(urlOtherThanClients(clientIP)){
+				var urlIPpair = randomUrlFromOtherClient(clientIP);
+				URLtoSend = urlIPpair[0] + "####Browser-Surprise####";
+				console.log("[+] " + URLtoSend + " originally visited by client " + urlIPpair[0] + " is sent to client " + clientIP);
+				
+				delete sitesByUser[urlIPpair[1]][urlIPpair[0]];
+				console.log("updated: \n" + sitesByUser);
+
+				deviceNewURLtimer[clientIP] = new Date().getTime() / 1000;
+			}
+			// URLtoSend = "http://www.artdelicorp.com/img2/browser-surprise.png####Browser-Surprise####";
+			// 
 			//update the timer:
-			deviceNewURLtimer[clientIP] = new Date().getTime() / 1000;
-		}else{
-			console.log('[-] request from ' + clientIP + " - too early");
+			
 		}
 	}
 
     res.writeHead(200, {"Content-Type": "text/plain"});
-    
     res.end(URLtoSend);
-    // 
 
 });
