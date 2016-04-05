c4710dc90a5e18110a38b7039dffcc54a570e393
Leon Eckert <eckertleon@gmail.com>
2016-04-05 17:45:28 -0400

    merged mine and melanies sound function

--- extension/playSurpriseSounds.js
+++ extension/playSurpriseSounds.js
@@ -1,11 +1,23 @@
+var audioFiles = [
+    'http://artdelicorp.com/audio/bs-00.m4a',
+    'http://artdelicorp.com/audio/bs-01.mp3',
+    'http://artdelicorp.com/audio/bs-02.mp3',
+    'http://artdelicorp.com/audio/bs-03.mp3',
+    'http://artdelicorp.com/audio/bs-04.mp3',
+    'http://artdelicorp.com/audio/bs-05.mp3'
+];
+
+var randomAudio = audioFiles[Math.floor(Math.random() * audioFiles.length)];
+
+
 function playSoundIfBrowserSurprise(){
 	var currentUrl = window.location.href;
-	if (currentUrl.length > 24){
+	if (currentUrl.length > 18){
 		var endOfURL = currentUrl.slice(currentUrl.length - 18, currentUrl.length);
-		console.log("endOfURL");
 		if(endOfURL == "##Browser-Surprise"){
-			var audioFile = new Audio('http://artdelicorp.com/audio/browser-surprise-audio.m4a');
+			var audioFile = new Audio(randomAudio);
 			audioFile.play();
 		}
 	}
 }
+
